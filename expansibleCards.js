83b7b4abd7317ad57ee13aca34dd199c7239c378
Clodomir <clovianna@gmail.com>
2023-03-11 04:29:45 -0300

    Improved UX feature.

--- expansibleCards.js
+++ expansibleCards.js
@@ -3,12 +3,16 @@ function expandSubject(card, subject){
     document.getElementById(card).style.transition = 'opacity 1s';
     document.getElementById(card).style.opacity = 0;
     document.getElementById(subject).hidden = true;
+
+    
     
     setTimeout(() => {
       
       document.getElementById(card).hidden = true;
       document.getElementById(subject).hidden = false;
       document.getElementById(subject).style.opacity = 1;
+
+      document.getElementById(subject).scrollIntoView();
   
     }, 1000); 
   
